df814f368c3759d41c6c0954906b374ac082eb18
Nico Schulthess <scnico@ethz.ch>
2021-10-08 13:51:17 +0200

    Enabled projection with black white images

--- projector.py
+++ projector.py
@@ -65,6 +65,8 @@ def project(
     target_images = target.unsqueeze(0).to(device).to(torch.float32)
     if target_images.shape[2] > 256:
         target_images = F.interpolate(target_images, size=(256, 256), mode='area')
+    if target_images.shape[1] == 1: # Number of channels == 1
+        target_images = target_images.repeat(1, 3, 1, 1)
     target_features = vgg16(target_images, resize_images=False, return_lpips=True)
 
     w_opt = torch.tensor(w_avg, dtype=torch.float32, device=device, requires_grad=True) # pylint: disable=not-callable
@@ -96,6 +98,8 @@ def project(
         synth_images = (synth_images + 1) * (255/2)
         if synth_images.shape[2] > 256:
             synth_images = F.interpolate(synth_images, size=(256, 256), mode='area')
+        if synth_images.shape[1] == 1: # Number of channels == 1
+            synth_images = synth_images.repeat(1, 3, 1, 1)
 
         # Features for synth images.
         synth_features = vgg16(synth_images, resize_images=False, return_lpips=True)
@@ -165,12 +169,19 @@ def run_projection(
         G = legacy.load_network_pkl(fp)['G_ema'].requires_grad_(False).to(device) # type: ignore
 
     # Load target image.
-    target_pil = PIL.Image.open(target_fname).convert('RGB')
+    target_pil = PIL.Image.open(target_fname)
+    n_channels = len(target_pil.getbands())
     w, h = target_pil.size
     s = min(w, h)
     target_pil = target_pil.crop(((w - s) // 2, (h - s) // 2, (w + s) // 2, (h + s) // 2))
     target_pil = target_pil.resize((G.img_resolution, G.img_resolution), PIL.Image.LANCZOS)
-    target_uint8 = np.array(target_pil, dtype=np.uint8)
+    if n_channels == 1:
+        target_uint8 = np.array(target_pil, dtype=np.uint8)[:,:,np.newaxis]
+    elif n_channels == 3:
+        target_uint8 = np.array(target_pil, dtype=np.uint8)
+    else:
+        print('Number of channels not in [1,3]')
+        return
 
     # Optimize projection.
     start_time = perf_counter()
@@ -201,7 +212,7 @@ def run_projection(
     synth_image = G.synthesis(projected_w.unsqueeze(0), noise_mode='const')
     synth_image = (synth_image + 1) * (255/2)
     synth_image = synth_image.permute(0, 2, 3, 1).clamp(0, 255).to(torch.uint8)[0].cpu().numpy()
-    PIL.Image.fromarray(synth_image, 'RGB').save(f'{outdir}/proj.png')
+    PIL.Image.fromarray(synth_image.squeeze()).save(f'{outdir}/proj.png')
     np.savez(f'{outdir}/projected_w.npz', w=projected_w.unsqueeze(0).cpu().numpy())
 
 #----------------------------------------------------------------------------
